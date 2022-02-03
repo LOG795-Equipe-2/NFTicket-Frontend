a558026673daaad610f1268e0e3dbf0945d2110a
Emmanuel Proulx <emmanuel.proulx@hotmail.com>
2022-02-03 13:48:26 -0500

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navigation from "./components/Navigation/Navigation";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3F72AF",
+      dark: "#112D4E",
+      light: "#DBE2EF",
+      contrastText: "#F9F7F7",
+    },
+    secondary: {
+      main: "#bb9c50",
+    },
+  },
+});
 
 function App() {
   return (
-    <div title="NFTicket App" className="App">
-      Landing page
-    </div>
+    <ThemeProvider theme={theme}>
+      <div title="NFTicket App" className="App">
+        <Navigation></Navigation>
+      </div>
+    </ThemeProvider>
   );
 }
 
