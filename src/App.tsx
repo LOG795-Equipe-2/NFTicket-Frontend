be3711d4732ec0227e73eb2865077df75fb50932
Anthony Brochu <anthony.brochu@outlook.com>
2022-02-08 16:51:12 -0500

    setup anchor to work with frontend app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import './App.css';
-import AnchorLink from 'anchor-link'
-import AnchorLinkBrowserTransport from 'anchor-link-browser-transport'
+import AnchorBrowserManager from './utils/AnchorBrowserManager';
+
+function performTransaction(manager: AnchorBrowserManager){
+  try{
+    manager.performTransactions([{
+      account: 'addressbook',
+          name: 'upsert',
+          data:{
+            user: 'alice',
+            first_name: 'alice',
+            last_name: 'liddell',
+            age: 24,
+            street: '123 drink me way',
+            city: 'wonderland',
+            state: 'amsterdam'
+        }
+      }, {
+        account: 'addressbook',
+        name: 'upsert',
+        data:{
+          user: 'alice',
+          first_name: 'alice',
+          last_name: 'liddell',
+          age: 25,
+          street: '123 drink me way',
+          city: 'wonderland',
+          state: 'amsterdam'
+        }
+      }]);
+
+  } catch(err) {
+    alert(err);
+  }
+}
 
 function App() {
-  const transport = new AnchorLinkBrowserTransport()
-  const link = new AnchorLink({transport, chains: [
-    {
-        chainId: '184ba45492aeb666274a46d82d7b212c5a79d888b4e4b6da31449765770410bf',
-        nodeUrl: 'http://localhost:8888',
-    }
-  ]})
-
-  console.log(link);
+  const manager = new AnchorBrowserManager('184ba45492aeb666274a46d82d7b212c5a79d888b4e4b6da31449765770410bf', 'http://localhost:8888', 'NFTicket');
+  
+  // Try to restore the session at beginning.
+  manager.restoreSession().then((value) => {
+    console.log("restored: " + value)
+  });
+  
 
   return (
     <div title="NFTicket App" className="App">
       Landing page
+      <button onClick={() => manager.login()}>Login</button>
+      <button onClick={() => performTransaction(manager)}>Execute Trx test</button>
+      <button onClick={() => manager.logout()}>Logout</button>
     </div>
   );
-}
-
-export default App;
+} export default App;
