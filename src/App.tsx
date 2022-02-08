518dfc93d5eb05920f4d036add76ca6520b77038
Anthony Brochu <anthony_g100@icloud.com>
2022-02-08 17:24:54 -0500

    put boiler plate in theme

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,11 @@ function App() {
   
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}> 
+      <button onClick={() => manager.login()}>Login</button>
+      <button onClick={() => performTransaction(manager)}>perform trx</button>
+      <button onClick={() => manager.logout()}>logout</button>
+
       <BrowserRouter>
         <div title="NFTicket App" className="App">
           <Navigation></Navigation>
