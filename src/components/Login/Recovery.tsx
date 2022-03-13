7ba0004681b5bb76bf5b56337067bf49e82c39ed
Naim Yahyaoui <43176171+NaimEts@users.noreply.github.com>
2022-03-12 19:18:40 -0500

    Update Recovery.tsx

--- src/components/Login/Recovery.tsx
+++ src/components/Login/Recovery.tsx
@@ -20,7 +20,10 @@ export default function Recovery() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    
+    console.log({
+      email: data.get('email')
+      
+    });
   };
 
   return (
