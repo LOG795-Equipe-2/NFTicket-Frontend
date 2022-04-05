1c0864bc3c2a8e2263db7714607ad3bf2b137c1c
Emmanuel Proulx <emmanuel.proulx@hotmail.com>
2022-04-04 20:53:25 -0400

    Add current progress for ticket purchase

--- src/components/BuyTicketView/BuyTicketView.tsx
+++ src/components/BuyTicketView/BuyTicketView.tsx
@@ -1,12 +1,24 @@
-import { Button, styled, Typography } from "@mui/material"
+import { Alert, Button, Snackbar, styled, Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import { Link, useParams } from "react-router-dom"
 import Event from "../../interfaces/Event";
 import TicketVisualiser from "../TicketVisualiser/TicketVisualiser";
+import NFTicketTransactionServiceInstance, { NFTicketTransactionService } from '../../services/NFTicketTransactionService';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import './BuyTicketView.scss';
+import { useEffect } from "react";
+import React from "react";
+
+let serviceNFT: NFTicketTransactionService
+
+async function connectToBackend() {
+  let service = NFTicketTransactionServiceInstance;
+  await service.init()
+  return service
+}
 
 export default function BuyTicketView() {
+  const [snackbarContent, setSnackbarContent] = React.useState<any>(null);
   const { id, ticketId } = useParams();
   const CssBox = styled(Box)(({ theme }) => ({
     ".BuyTicket": {
@@ -32,7 +44,7 @@ export default function BuyTicketView() {
     dateTime: new Date(),
     ticketCategories: [
       {
-        id: "1",
+        id: "6245ce6ecc4fed483329",
         type: "Standard",
         price: 20.00,
         initialAmount: 100,
@@ -46,7 +58,7 @@ export default function BuyTicketView() {
         }
       },
       {
-        id: "2",
+        id: "6243166810eaee472ab6",
         type: "VIP",
         price: 30.00,
         initialAmount: 20,
@@ -61,16 +73,42 @@ export default function BuyTicketView() {
       }
     ]
   };
+  useEffect(() => {
+    connectToBackend().then((service) => {
+      // Try to restore the session at beginning.
+      // service.getManager().restoreSession().then((value) => {
+      //   console.log("restored session?: " + value)
+      // })
+      serviceNFT = service
+    });
+  }, []); // checks for changes in the values in this array
   const ticket = event.ticketCategories.find(ticketCategory => ticketCategory.id === ticketId);
-  const processTicketTransaction = () => {
+  const processTicketTransaction = async () => {
     // TODO: Call Anchor API, update remaining QTY on success
+    if (!!ticketId) {
+      try {
+        let transactionObject = await serviceNFT.buyTicketFromCategory(ticketId);
+        let validationResponse = await serviceNFT.validateBuyTicketFromCategory(transactionObject);
+        console.log(validationResponse)
+      } catch (e: any) {
+        console.log(e);
+        setSnackbarContent({ type: "error", message: "Une erreur s'est produite dans la transaction, veuillez réessayer plus tard." })
+      }
+
+    }
+
   }
   return (
     ticket ?
       <CssBox className="BuyTicket">
+        <Snackbar open={!!snackbarContent} autoHideDuration={6000} onClose={() => setSnackbarContent(null)}>
+          {snackbarContent && (
+            <Alert onClose={() => setSnackbarContent(null)} severity={snackbarContent.type}>{snackbarContent.message}</Alert>
+          )}
+        </Snackbar>
         <div className="BuyTicket__content">
           <div className="go-back">
-            <Button variant="outlined" color="primary"><Link to={"/events/" + id}><ArrowBackIcon/> Retourner à l'événement</Link></Button>
+            <Button variant="outlined" color="primary"><Link to={"/events/" + id}><ArrowBackIcon /> Retourner à l'événement</Link></Button>
           </div>
           <TicketVisualiser ticket={ticket} event={event} size="large" />
           <div className="cost">
@@ -78,7 +116,7 @@ export default function BuyTicketView() {
             <Typography className="text" variant="h4">{ticket.price.toFixed(2)} $</Typography>
           </div>
           <div className="confirm">
-            <Button onClick={processTicketTransaction} variant="contained" color="success">Confirmer la transaction</Button>
+            <Button onClick={processTicketTransaction} variant="contained" color="info">Confirmer la transaction</Button>
           </div>
         </div>
       </CssBox> :
