6bdf16e22964a3708481d250306003bbe19b0e94
zaclimon <isaacpateau05@gmail.com>
2022-04-10 12:56:31 -0400

    Add dummy test

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -116,7 +116,7 @@ function Navigation() {
               ) : (typeof (value.userLoggedIn?.username) == "undefined" ?
                 <div className="navbar__right__login">
                   <CssLink href="sign-in" underline="none">
-                    Connexion/Inscription
+                    Test
                     <AccountCircleOutlinedIcon></AccountCircleOutlinedIcon>
                   </CssLink>
                 </div>
